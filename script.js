414f367f6d1e501c4c6eca5ad1d3b683995d8a1c
jonai whitehorn <jonaidancer@gmail.com>
2017-12-14 11:23:48 -0600

    adding tweaks and npm update

--- script.js
+++ script.js
@@ -13,3 +13,44 @@ else
 
 bounceBubbles();
 
+$(document).ready(function() {
+  $('#send').click(function(e) {
+    e.preventDefault();
+    $(this).addClass('animated fadeOutRight');
+    $('.box form').addClass('animated fadeOut');
+
+    $(this, '.box form').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd  oanimationend animationend', function() {
+      $(this).hide();
+      $('.box form').hide();
+      $('.thanks').show().addClass('animated zoomIn');
+      $('.box').height('auto');
+    });
+
+  })
+
+})
+
+
+$('.ml2').each(function(){
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml2 .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: function(el, i) {
+      return 70*i;
+    }
+  }).add({
+    targets: '.ml2',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
