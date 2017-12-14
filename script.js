b8c1c21ec6849169345780654c7a85392d912326
jonai whitehorn <jonaidancer@gmail.com>
2017-12-13 20:57:54 -0600

    adding stuff

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 function submitContactForm(){
         alert('Message has been sent!');
 }
-var myName = "Jonai ";
-
-var red = [0, 100, 63];
-var orange = [40, 100, 60];
-var green = [75, 100, 40];
-var pink = [#ADFF2F];
-var purple = [280, 50, 60];
-var letterColors = [red, orange, green, blue, purple];
-
-drawName(myName, letterColors);
 
 if(10 < 3)
 {
@@ -22,3 +12,4 @@ else
 }
 
 bounceBubbles();
+
